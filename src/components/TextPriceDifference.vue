dc9e6966dcb2e12b1a82d46ed688e26f6b1f5674
ngseke <ngseke@gmail.com>
2023-10-01 15:54:25 +0800

    feat: update colors

--- src/components/TextPriceDifference.vue
+++ src/components/TextPriceDifference.vue
@@ -3,22 +3,25 @@ import { computed } from 'vue'
 import { faArrowUp, faArrowDown, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { formatPrice } from '../modules/price'
+import { useDark } from '../composables/useDark'
 
 const props = defineProps<{
   value?: number | null
 }>()
 
+const isDark = useDark()
+
 const icon = computed(() => {
   if (props.value == null) return
   if (props.value > 0) {
     return {
-      className: 'text-red-500',
+      className: isDark.value ? 'text-rose-600' : 'text-rose-700',
       icon: faArrowUp,
     }
   }
   if (props.value < 0) {
     return {
-      className: 'text-green-500',
+      className: isDark.value ? 'text-lime-600' : 'text-lime-700',
       icon: faArrowDown,
     }
   }
