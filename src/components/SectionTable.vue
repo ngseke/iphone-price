ff14fa592897de783b3c7ca8866e7f43319b66a7
ngseke <ngseke@gmail.com>
2024-09-01 17:58:39 +0800

    feat: add adjusted price

--- src/components/SectionTable.vue
+++ src/components/SectionTable.vue
@@ -10,6 +10,7 @@ import { type StorageSize } from '../types/StorageSize'
 import { formatPrice } from '../modules/price'
 import { formatStorageSize } from '../modules/storageSize'
 import { formatIphoneModel } from '../modules/iphoneModel'
+import AdjustedPrice from './AdjustedPrice.vue'
 
 const groups = groupBy(
   iphoneList.filter(iphone => iphone.isInitialRelease),
@@ -35,12 +36,17 @@ const rows = computed(() => {
   })
   return Object.values(map)
 })
+
+function findAdjustedList (storage: StorageSize, model: IphoneModel) {
+  return iphoneList
+    .filter(iphone => !iphone.isInitialRelease && iphone.storage === storage && iphone.model === model)
+}
 </script>
 
 <template>
   <section class="mx-auto flex max-w-5xl flex-col gap-y-8">
     <h2 class="text-3xl font-bold">
-      歷年發售機型
+      歷年首發機型
     </h2>
 
     <div class="flex flex-wrap gap-4">
@@ -64,10 +70,13 @@ const rows = computed(() => {
       </div>
 
       <div class="min-w-full sm:mx-0 sm:flex-1 lg:min-w-0">
-        <div class="flex w-full flex-col items-start space-y-8 overflow-x-auto lg:sticky lg:top-8 lg:px-8">
-          <div class="w-full min-w-[36rem]">
-            <div class="overflow-x-auto">
-              <table class="table">
+        <div class="flex w-full flex-col items-start space-y-8 lg:sticky lg:top-8 lg:px-8">
+          <h3 class="text-xl font-medium">
+            {{ formatDateChinese(selectedDate) }}
+          </h3>
+          <div class="w-full min-w-0">
+            <div class="overflow-x-auto overflow-y-hidden ">
+              <table class="table mb-4">
                 <thead>
                   <tr>
                     <th>型號</th>
@@ -78,15 +87,29 @@ const rows = computed(() => {
                 </thead>
                 <tbody>
                   <tr v-for="(row, key) in rows" :key="key" class="hover">
-                    <td>{{ formatIphoneModel(Object.values(row)[0]?.model) }}</td>
-                    <td v-for="column in columns" :key="column">
-                      <span class="font-rubik text-lg font-bold">
-                        {{ formatPrice(row[column]?.price.twd) }}
-                      </span>
+                    <td class="font-bold">{{ formatIphoneModel(Object.values(row)[0]?.model) }}</td>
+                    <td v-for="column in columns" :key="column" class="align-top">
+                      <div class="flex flex-col items-start">
+                        <span class="font-rubik text-lg font-bold">
+                          {{ formatPrice(row[column]?.price.twd) }}
+                        </span>
+
+                        <AdjustedPrice
+                          v-for="(iphone, index) in findAdjustedList(column, Object.values(row)[0]?.model)"
+                          :key="index"
+                          :original="
+                            findAdjustedList(column, Object.values(row)[0]?.model)[index - 1]?.price.twd ??
+                              row[column]?.price.twd
+                          "
+                          :releasedAt="iphone.releasedAt"
+                          :value="iphone?.price.twd"
+                        />
+                      </div>
                     </td>
                   </tr>
                 </tbody>
               </table>
+              <span class="text-xs opacity-70">單位：新台幣</span>
             </div>
           </div>
         </div>
