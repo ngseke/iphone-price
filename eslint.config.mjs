25c3a38d8da89808a528eadac4dee4861bd53bbd
ngseke <ngseke@gmail.com>
2025-09-11 22:21:46 +0800

    chore: set up linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,33 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import tailwind from 'eslint-plugin-tailwindcss'
+import js from '@eslint/js'
+import ts from 'typescript-eslint'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
     ],
   },
-];
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...tailwind.configs['flat/recommended'],
+  eslintPluginPrettierRecommended,
+]
 
-export default eslintConfig;
+export default eslintConfig
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ['./**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
