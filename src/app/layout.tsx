b3c44a324f146472d5727bb13f9d3c18a79a8886
ngseke <ngseke@gmail.com>
2025-09-12 21:02:50 +0800

    feat: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,25 @@ const rubik = Rubik({
   subsets: ['latin'],
 })
 
+const title = '台灣 iPhone 價格歷史趨勢'
+const description = '透過圖表比較 iPhone 17 和歷代的價格變化'
+const ogImage = 'https://iphone-price.ngseke.me/og-image.png'
+
 export const metadata: Metadata = {
-  title: '2025 年台灣 iPhone 價格歷史趨勢可視化',
-  description: '',
+  title,
+  openGraph: {
+    title,
+    description,
+    siteName: title,
+    url: 'https://iphone-price.ngseke.me/',
+    images: [{ url: ogImage, width: 1320, height: 840 }],
+  },
+  twitter: {
+    title,
+    images: [{ url: ogImage }],
+    card: 'summary_large_image',
+  },
+  description,
   icons: {
     icon: [{ url: '/favicon.svg', type: 'image/svg+xml' }],
   },
